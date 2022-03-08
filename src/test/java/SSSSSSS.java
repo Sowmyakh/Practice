da790dbfd380d529dc890d23d3b3b15f06ed17a4
AvinasJI17 <avi3k16@gmail.com>
2022-03-08 15:38:36 +0530

    new modified

--- src/test/java/SSSSSSS.java
+++ src/test/java/SSSSSSS.java
@@ -4,4 +4,10 @@ private void syso() {
 	// TODO Auto-generated method stub
 System.out.println("hi...................");
 }
+
+public void newModification(){
+    System.out.println("new modification");
+    System.out.println("new modification1");
+    System.out.println("new modification2");
+}
 }
